ce4909f741657da8871c275b718584c6c94f02a7
Matheus Elias Cruz <matheus.elias.cruz@hotmail.com>
2022-08-19 13:39:24 -0300

    Adição dos repositories de Vendedor e Atuacao

---
+++ src/main/java/br/com/matheliasc/Vendedores/repository/AtuacaoRepository.java
@@ -0,0 +1,6 @@
+package br.com.matheliasc.Vendedores.repository;
+
+import br.com.matheliasc.Vendedores.model.Atuacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AtuacaoRepository extends JpaRepository<Atuacao,String> {}
---
+++ src/main/java/br/com/matheliasc/Vendedores/repository/VendedorRepository.java
@@ -0,0 +1,6 @@
+package br.com.matheliasc.Vendedores.repository;
+
+import br.com.matheliasc.Vendedores.model.Vendedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VendedorRepository extends JpaRepository<Vendedor,Integer> {}
