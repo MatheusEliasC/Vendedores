66d907b59a110284f0c8ae6ae77fcb1c43967791
Matheus Elias Cruz <matheus.elias.cruz@hotmail.com>
2022-08-19 13:16:43 -0300

    Primeiro commit, archetype spring initializer

---
+++ src/test/java/br/com/matheliasc/Vendedores/VendedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheliasc.Vendedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
