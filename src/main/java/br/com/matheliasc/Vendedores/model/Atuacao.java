6b3a0d34f8c098c3360ff793c587c739fe1f7189
Matheus Elias Cruz <matheus.elias.cruz@hotmail.com>
2022-08-19 13:40:04 -0300

    Configuração da conexão com banco de dados h2

--- src/main/java/br/com/matheliasc/Vendedores/model/Atuacao.java
+++ src/main/java/br/com/matheliasc/Vendedores/model/Atuacao.java
@@ -2,11 +2,15 @@ package br.com.matheliasc.Vendedores.model;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.List;
 
 @Data
+@Entity
 public class Atuacao {
 
+    @Id
     private String regiao;
     private List<String> estados;
 }
