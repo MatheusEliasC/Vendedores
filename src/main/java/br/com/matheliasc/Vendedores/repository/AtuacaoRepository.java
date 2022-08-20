12ac6ac797603c2c41059908971bae948ef4233a
Matheus Elias Cruz <matheus.elias.cruz@hotmail.com>
2022-08-20 16:57:15 -0300

    Adição da busca de Atuacao por regiao no repository

--- src/main/java/br/com/matheliasc/Vendedores/repository/AtuacaoRepository.java
+++ src/main/java/br/com/matheliasc/Vendedores/repository/AtuacaoRepository.java
@@ -3,4 +3,10 @@ package br.com.matheliasc.Vendedores.repository;
 import br.com.matheliasc.Vendedores.model.Atuacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AtuacaoRepository extends JpaRepository<Atuacao,String> {}
+import java.util.Optional;
+
+public interface AtuacaoRepository extends JpaRepository<Atuacao,String> {
+
+    Optional<Atuacao> findByRegiao (String regiao);
+
+}
